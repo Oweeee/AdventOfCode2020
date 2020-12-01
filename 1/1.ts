30bb48221b94f1b4c5d21241aa05d62633c19da5
Oskar Willman <oskar@insertcoin.se>
2020-12-02 00:07:42 +0100

    fix

--- 1/1.ts
+++ 1/1.ts
@@ -1,8 +1,9 @@
 function findTwoAndMultiply(entries: number[]) {
   for (let i = 0; i < entries.length; i++) {
     for (let j = i + 1; j < entries.length; j++) {
-      if (entries[i] + entries[j] === 2020)
-        console.log(entries[i] * entries[j]);
+      if (entries[i] + entries[j] === 2020) {
+        return entries[i] * entries[j];
+      }
     }
   }
   return false;
@@ -14,7 +15,7 @@ function findThreeAndMultiply(entries: number[]) {
       if (entries[i] + entries[j] >= 2020) continue;
       for (let k = j + 1; k < entries.length; k++) {
         if (entries[i] + entries[j] + entries[k] === 2020)
-          console.log(entries[i] * entries[j] * entries[k]);
+          return entries[i] * entries[j] * entries[k];
       }
     }
   }
